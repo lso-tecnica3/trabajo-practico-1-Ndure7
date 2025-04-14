ddba8fb169c2d41308ab82e83dc0ee65eb975421
Ndure7 <ludmilanairdure2@gmail.com>
2025-04-14 11:30:51 -0300

    Entrega ejercicio 3

---
+++ tp1-3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+    <title> TP1 - Ejercicio 3 </title>
+</head>
+
+<?php
+// Bucle que va desde 10 hasta 100 en pasos de 10 para usarlo despues como tamaño
+for($tamaño = 10; $tamaño <= 100; $tamaño += 10) {
+    print "<p style='font-size: " . $tamaño . "px'>Hola mundo</p>";
+}
+?>
+
+</body>
+</html>
